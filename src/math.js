e5dfbf01a7d97a5559a74b5748cac7eab47e226e
Alejandro Reyes <reyesbalejandroe@gmail.com>
2022-10-05 11:08:38 +0200

    add/calculatePercentage

--- src/math.js
+++ src/math.js
@@ -2,5 +2,6 @@ const sum = (a, b) => a + b;
 const mul = (a, b) => a * b;
 const sub = (a, b) => a - b;
 const div = (a, b) => a / b;
+const calculatePercentage = (num, percentage) => (num*percentage)/100
 
 module.exports = { sum, mul, sub, div };
