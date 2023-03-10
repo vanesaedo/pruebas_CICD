67d2283e06965c20bd3d94befc2a5c07e60ccc82
Alejandro Reyes <reyesbalejandroe@gmail.com>
2023-03-10 11:25:20 +0100

    add/semicolons_cpertange

--- __test__/math.test.js
+++ __test__/math.test.js
@@ -8,10 +8,10 @@ test('Multiplying 1 * 1 equals 1', () => {
 });
 test('Subtracting 1 - 1 equals 0', () => {
   expect(sub(1, 1)).toBe(0);
-})
+});
 test('Dividing 1 / 1 equals 1', () => {
   expect(div(1, 1)).toBe(1);
-})
+});
 test('Dividing 1/0 equals infinite',()=>{
   expect(div(1, 0)).toBe(Infinity);
-})
+});
--- src/math.js
+++ src/math.js
@@ -4,4 +4,4 @@ const sub = (a, b) => a - b;
 const div = (a, b) => a / b;
 const calculatePercentage = (num, percentage) => (num*percentage)/100;
 
-module.exports = { sum, mul, sub, div };
+module.exports = { sum, mul, sub, div, calculatePercentage };
