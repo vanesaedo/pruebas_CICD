6b5ff52afdfb62bad4d881bd05cf7ef5e4fec9e8
Vanesa Edo <vanesa.en@gmail.com>
2024-05-09 11:47:05 +0200

    math.test.js errors added

--- __test__/math.test.js
+++ __test__/math.test.js
@@ -4,10 +4,10 @@ test('Adding 1 + 1 equals 2', () => {
   expect(sum(1, 1)).toBe(2);
 });
 test('Multiplying 1 * 1 equals 1', () => {
-  expect(mul(1, 1)).toBe(1);
+  expect(mul(1, 1)).toBe(666);
 });
 test('Subtracting 1 - 1 equals 0', () => {
-  expect(sub(1, 1)).toBe(0);
+  expect(sub(1, 1)).toBe(777);
 });
 test('Dividing 1 / 1 equals 1', () => {
   expect(div(1, 1)).toBe(1);
