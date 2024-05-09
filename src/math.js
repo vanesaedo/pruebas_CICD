6ad8668dcee81741c774781a9d9754505cc629f7
Vanesa Edo <vanesa.en@gmail.com>
2024-05-09 11:44:13 +0200

    fixed

--- src/math.js
+++ src/math.js
@@ -1,7 +1,7 @@
 const sum = (a, b) => a + b;
 const mul = (a, b) => a * b;
-const sub = (a, b) => a - b
-const div = (a, b) => a / b
+const sub = (a, b) => a - b;
+const div = (a, b) => a / b;
 const calculatePercentage = (num, percentage) => (num*percentage)/100;
 
 module.exports = { sum, mul, sub, div, calculatePercentage };
